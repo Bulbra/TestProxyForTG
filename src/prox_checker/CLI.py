dd194a949259d760af7c460b7fc76591f4781523
Bulbra <darthartem76@gmail.com>
2026-07-29 12:40:59 +0300

    ю

--- src/prox_checker/CLI.py
+++ src/prox_checker/CLI.py
@@ -9,8 +9,6 @@ from .main import run, load_proxies
 from typing_extensions import Annotated
 import typer
 
-
-
 load_dotenv()
 # https://api.telegram.org/bot{TOKEN}/getMe
 TOKEN = os.getenv("BOT_TOKEN")
